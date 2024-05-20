24601f8fa60d80e3bcd936d6f86775b89979541b
7erp1c <ul_tray@bk.ru>
2024-05-21 00:08:25 +0300

    new project Nest

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  Provider,
+} from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { appSettings } from './settings/app-settings';
+import { UsersRepository } from './features/users/infrastructure/users.repository';
+import { UsersService } from './features/users/application/users.service';
+import { UsersQueryRepository } from './features/users/infrastructure/users.query-repository';
+import { User, UserSchema } from './features/users/domain/user.entity';
+import { UsersController } from './features/users/api/users.controller';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+
+const usersProviders: Provider[] = [
+  UsersRepository,
+  UsersService,
+  UsersQueryRepository,
+];
+
+@Module({
+  // Регистрация модулей
+  imports: [
+    MongooseModule.forRoot(appSettings.api.MONGO_CONNECTION_URI),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  // Регистрация провайдеров
+  providers: [...usersProviders,],
+  // Регистрация контроллеров
+  controllers: [UsersController],
+})
+export class AppModule implements NestModule {
+  // https://docs.nestjs.com/middleware#applying-middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+// Custom guard
+// https://docs.nestjs.com/guards
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    if (request.query['token'] !== '123') {
+      // Если нужно выкинуть custom ошибку с кодом 401
+      // throw new UnauthorizedException();
+
+      // default error 403
+      return false;
+    }
+
+    return true;
+  }
+}
